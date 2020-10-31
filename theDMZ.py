04e2686d9426137336c208d27c8ebdaa55bd7f28
ParetoYXE <35578020+ParetoYXE@users.noreply.github.com>
2020-10-31 18:34:19 -0500

    Complete rebuild of the engine
    Worked on a complete rebuild of the engine. This is skeleton code and only implements rudimentary map generation, user movement and rendering.

--- theDMZ.py
+++ theDMZ.py
@@ -1,241 +1,207 @@
-import pygame,random
+import pygame, random, math
 pygame.init()
 
 
-gameDisplay = pygame.display.set_mode((900,900))
+
+
+gameDisplay = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
 pygame.display.set_caption('The DMZ')
 pygame.font.init() # you have to call this at the start, 
                    # if you want to use this module.
-myfont = pygame.font.SysFont('VCR_OSD_MONO', 20)
+myfont = pygame.font.SysFont('VCR_OSD_MONO', 40,)
 clock = pygame.time.Clock()
+
+w, h = pygame.display.get_surface().get_size()
 
 quit = False
 
+overWorld = []
+localMap = [0,60,0,50]
+mapX = math.floor(w * 1/4)
+mapY = math.floor(h * 1/5)
+
+hutImage = pygame.image.load("Hut.png")
+treeImage = pygame.image.load("tree.png")
+bannerImage = pygame.image.load("Banner.png")
+playerImage = pygame.image.load("player.png")
+waterImage = pygame.image.load("water.png")
+marshImage = pygame.image.load("marshLand.png")
+grassImage = pygame.image.load("grass.png")
+settlementImage = pygame.image.load("Settlement.png")
+npcImage = pygame.image.load("npc.png")
 
 
 
-#DECLARE GLOBAL ARRAYS HERE
+playerLocation = [20,20]
+stats = {"Strength":0,"Dexterity":0,"Constitution":0,"Intelligence":0,"Charisma":0,
+		 "Cyber":0,"HP":0,"Toughness":0,"Influence":0,"Leadership":0}
+
 
 entities = []
-env = []
 
 
 
+class entity:
 
-
-
-
-
-
-#DECLARE GLOBAL STAT DISPLAY VARIABLES HERE
-
-selectedStr = 0
-selectedDex = 0
-selectedCon = 0
-selectedInt = 0 
-selectedCha = 0 
-selectedCyber = 0
-selectedHp = 0
-selectedTo = 0
-selectedLe = 0
-selectedInf = 0
-
-
-
-#LOAD IMAGES HERE
-
-npcImage = pygame.image.load("npc.png")
-brushImage = pygame.image.load("brush.png")
-houseImage = pygame.image.load("house.png")
-
-
-
-
-
-
-
-class Background(pygame.sprite.Sprite):
-    def __init__(self, image_file, location):
-        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
-        self.image = pygame.image.load(image_file)
-        self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = location
-
-
-
-
-class ENV():
-	def __init__(self):
-		self.name = ''
-		self.x = 0
-		self.y = 0
-		self.value = 0
-		self.state = ''
-		self.ownership = ''
-		self.image = ''
-
-
-
-class NPC():
-	def __init__(self):
-		self.name = ''
-		self.age = ''
-		self.str = 0
-		self.dex = 0
-		self.con = 0
-		self.int = 0
-		self.char = 0 
-		self.cyber = 0
+	def __init__():
+		self.s = 0
+		self.d = 0
+		self.c = 0
+		self.i = 0
+		self.ch = 0
+		self.cy = 0
 		self.hp = 0
-		self.to = 0
-		self.le = 0
+		self.t = 0 
 		self.inf = 0
-		self.x = 0
-		self.y = 0
-		self.image ="npc"
-		self.width = 10
-		self.height = 10
+		self.l = 0
 
-player = NPC()
-
-player.str = random.randint(3,18)
-player.dex = random.randint(3,18)
-player.con = random.randint(3,18)
-player.int = random.randint(3,18)
-player.char = random.randint(3,18)
-player.cyber = random.randint(3,18)
-player.hp = random.randint(3,18)
-player.to = random.randint(3,18)
-player.le = random.randint(3,18)
-player.inf = random.randint(3,18)
-
-
-
-def generateENV():
-
-	genENV = ENV()
-
-	if(random.randint(1,10) < 3):
-		genENV.image ="house"
-	else:
-		genENV.image ="brush"
-	genENV.x = random.randint(100,800)
-	genENV.y = random.randint(100,800)
-
-	return genENV
-
-def generateNPC():
-	genNPC = NPC()
-
-	genNPC.str = random.randint(3,18)
-	genNPC.dex = random.randint(3,18)
-	genNPC.con = random.randint(3,18)
-	genNPC.int = random.randint(3,18)
-	genNPC.char = random.randint(3,18)
-	genNPC.cyber = random.randint(3,18)
-	genNPC.hp = genNPC.str + genNPC.con
-	genNPC.to = (genNPC.str + genNPC.con) / 2
-	genNPC.le = (genNPC.char + genNPC.int) / 2
-	genNPC.inf = (genNPC.cyber + genNPC.char) / 2
-	genNPC.x = random.randint(100,800)
-	genNPC.y = random.randint(100,800)
-
-	return genNPC
+		
+def genMap():
+	for i in range(700):
+		line = []
+		lineE = []
+		for j in range(400):
+			line.append(random.randint(1,1600))
+			lineE.append(0)
+		overWorld.append(line)
+		entities.append(lineE)
+	for i in range(600):
+		for j in range(300):
+			#Seed river
+			if(overWorld[i][j] == 401):
+				northSouth = False
+				count = 0
+				direction = 0
+				pivot = 0
+				for k in range(random.randint(10,50)):
+					twist = (random.randint(0,10))
+					if(twist < 2):
+						direction = random.randint(1,2)
+						pivot = k
+						
+					if(j+k<len(overWorld[i])):
+						if(direction == 1):
+							overWorld[i+k][j+pivot+random.randint(0,1)] = 400
+						elif(direction == 2):
+							overWorld[i+pivot+random.randint(0,1)][j+k] = 400
 
 
-def drawStats():
-	strText = myfont.render('STR: ' + str(selectedStr), False, (0, 0, 0))
-	dexText = myfont.render('DEX: ' + str(selectedDex), False, (0, 0, 0))
-	conText = myfont.render('CON: ' + str(selectedCon), False, (0, 0, 0))
-	intText = myfont.render('INT: ' + str(selectedInt), False, (0, 0, 0))
-	charText = myfont.render('CHAR: ' + str(selectedCha),False, (0, 0, 0))
-	cyberText = myfont.render('CYBER: ' + str(selectedCyber),False, (0, 0, 0))
-	hpText = myfont.render('HP: ' + str(selectedHp),False, (0, 0, 0))
-	toText = myfont.render('TOUGH: ' + str(selectedTo),False, (0, 0, 0))
-	leText = myfont.render('LEAD: ' + str(selectedLe),False, (0, 0, 0))
-	infText = myfont.render('INFL: ' + str(selectedInf),False, (0, 0, 0))
+			#Seed lake
+			if(overWorld[i][j]==406):
+				for k in range(100):
+					overWorld[i+random.randint(1,5)][j+random.randint(1,5)] = 400
+					overWorld[i+random.randint(1,5)][j+random.randint(1,5)] = 400
+			#Seed Marsh
+			if(overWorld[i][j]==400):
+				if(overWorld[i][j-1] != 400):
+					overWorld[i][j-1] = 605
+				if(overWorld[i][j+1] != 400):
+					overWorld[i][j+1] = 605
+				if(overWorld[i-1][j] != 400):
+					overWorld[i-1][j] = 605
+				if(overWorld[i+1][j] != 400):
+					overWorld[i+1][j] = 605
+			
+			#seed settlements
+			if(overWorld[i][j] == 405):
+				city = False
+				for l in range(20):
+					if((overWorld[i-l][j] == 400) or (overWorld[i+l][j] == 400) or (overWorld[i][j-l] == 400) or (overWorld[i][j+l] == 400)):
+						print("city")
+						entities[i][j] = 1
+						city = True
+						for k in range(random.randint(10,50)):
+							overWorld[i+random.randint(-5,5)][j+random.randint(-5,5)] = 1
+						break
+				if(not city):
+					for k in range(random.randint(3,10)):
+						overWorld[i+random.randint(-4,3)][j+random.randint(-3,3)] = 1
 
-	gameDisplay.blit(strText,(0,800))
-	gameDisplay.blit(dexText,(0,820))
-	gameDisplay.blit(conText,(0,840))
-	gameDisplay.blit(intText,(0,860))
-	gameDisplay.blit(charText,(0,880))
-	gameDisplay.blit(cyberText,(60,800))
-	gameDisplay.blit(hpText,(60,820))
-	gameDisplay.blit(toText,(60,840))
-	gameDisplay.blit(leText,(60,860))
-	gameDisplay.blit(infText,(60,880))
+	print(entities)
 
-
-def updateStats(NPC):
-	global selectedStr,selectedDex,selectedCon,selectedInt,selectedInf,selectedCha,selectedCyber,selectedHp,selectedLe,selectedInf,selectedTo
-
-	selectedStr = NPC.str
-	selectedDex = NPC.dex
-	selectedCon = NPC.con
-	selectedInt = NPC.int 
-	selectedCha = NPC.char
-	selectedCyber = NPC.cyber
-	selectedHp = NPC.hp
-	selectedTo = NPC.to
-	selectedLe = NPC.le
-	selectedInf = NPC.inf
-
-def drawNPCS():
-	for i in entities:
-		if i.image == "npc":
-			gameDisplay.blit(npcImage,(i.x,i.y))
+def renderStats():
+	Y = 0
+	for i in stats:
+		textsurface = myfont.render(i + ": "+str(stats[i]), False, (255, 255, 255))
+		gameDisplay.blit(textsurface,(w*3/4,h*1/4+Y))
+		Y+=40
 
 
-def drawEnv():
-	for i in env:
-		if i.image == "brush":
-			gameDisplay.blit(brushImage,(i.x,i.y))
-		elif i.image == "house":
-			gameDisplay.blit(houseImage,(i.x,i.y))
+def renderLocalMap():
+	tileX = mapX
+	tileY = mapY
+	for i in range(localMap[2],localMap[3]):
+		tileX = mapX
+		for j in range(localMap[0],localMap[1]):
+			if(i == playerLocation[1] and j == playerLocation[0]):
+				gameDisplay.blit(playerImage,(tileX,tileY))
+			else:
+				if(overWorld[i][j]>1200):
+					gameDisplay.blit(treeImage,(tileX,tileY))
+				elif(overWorld[i][j] == 400):
+					gameDisplay.blit(waterImage,(tileX,tileY))
+				elif(overWorld[i][j]==405):
+					gameDisplay.blit(settlementImage,(tileX,tileY))
+				elif(overWorld[i][j]==1):
+					gameDisplay.blit(hutImage,(tileX,tileY))
+				elif(overWorld[i][j]==605):
+					gameDisplay.blit(marshImage,(tileX,tileY))
+				else:
+					gameDisplay.blit(grassImage,(tileX,tileY))
 
-def mouseCollisionDetection(pos):
-	for i in entities:
-		if(i.x <= pos[0] < i.x + i.width):
-			if(i.y <= pos[1] <= i.y + i.height):
-				updateStats(i)
+				if(entities[i][j]==1):
+					gameDisplay.blit(npcImage,(tileX,tileY))
 
 
 
+			tileX +=16
+		tileY+=16
 
 
+genMap()
+renderLocalMap()
 
-for i in range(10):
-	genNPC = generateNPC()
-	entities.append(genNPC)
-
-
-for i in range(30):
-	genENV = generateENV()
-	env.append(genENV)
-
-
-
-entities.append(player)
 
 
 while not quit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-        	pos = pygame.mouse.get_pos()
-        	mouseCollisionDetection(pos)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			quit = True
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			pos = pygame.mouse.get_pos()
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				quit = True
+			elif event.key == pygame.K_d:
+				localMap[0]+=1
+				localMap[1]+=1
+				playerLocation[0]+=1
+			elif event.key == pygame.K_w:
+				if(localMap[2]>0):
+					localMap[2]-=1
+					localMap[3]-=1
+					playerLocation[1]-=1
+			elif event.key == pygame.K_s:
+				if(localMap[3]<=700):
+					localMap[2]+=1
+					localMap[3]+=1
+					playerLocation[1] +=1
+			elif event.key == pygame.K_a:
+				if(localMap[0]>0):
+					localMap[0] -=1
+					localMap[1] -=1
+					playerLocation[0]-=1
 
 
+	gameDisplay.fill([0,0,0])
+	renderLocalMap()
+	renderStats()
+	pygame.display.update()
+	clock.tick(60)
 
-    gameDisplay.fill([255, 255, 255])
-    drawNPCS()
-    drawEnv()
-    drawStats()
-    pygame.display.update()
-    clock.tick(60)
 
 
 pygame.quit()
-quit()
+
+
+
